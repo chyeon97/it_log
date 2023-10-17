a7b1e882cd12871d7c33212528bef4abb02708ad
chaeyeon <pcyeon07@douzone.com>
2023-10-17 17:49:28 +0900

    feat: 카테고리 및 포스트 샘플 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,14 @@ import styled from '@emotion/styled';
 import GlobalStyle from 'components/common/GlobalStyle';
 import Introduction from 'components/main/Introduction';
 import Footer from 'components/common/Footer';
+import CategoryList from 'components/main/CategroyList';
+import PostList from 'components/main/PostList';
 
+const CATEGORY_LIST = {
+  All: 5,
+  Web: 3,
+  Mobile: 2,
+}
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -15,6 +22,9 @@ const IndexPage: FunctionComponent = function () {
     <Container>
       <GlobalStyle />
       <Introduction />
+
+      <CategoryList  selectedCategory='Web' categoryList={CATEGORY_LIST}/>
+      <PostList/>
       <Footer />
     </Container>
   )
